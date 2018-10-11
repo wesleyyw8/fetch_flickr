007530674422f8ccba8eaaa1ea9688f538356af9
Wesley Rebelo <wrebelo@cartrawler.com>
2018-10-11 17:22:13 +0100

    basic structure

--- logic.js
+++ logic.js
@@ -1,17 +1,9 @@
-function makeAJAXRequest () {
-    var request = new XMLHttpRequest();
-    var url = "//api.flickr.com/services/rest/?method=flickr.photos.search&api_key=<APIKEYHERE>&format=json&nojsoncallback=?&text=";
-    //var url = '//api.flickr.com/services/feeds/photos_public.gne?format=json&tags=puppy';
-    request.addEventListener("load", writeResponse);
+var s = document.createElement("script");
+s.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=puppy";
+document.body.appendChild(s);
 
-    // request.open("GET", url + document.getElementById("query").value);
-    request.open("GET", url);
-    request.send();
+//makeAJAXRequest();
+
+function jsonFlickrFeed (data) {
+  console.log(data);
 }
-
-function writeResponse() {
-  resultsArray = JSON.parse(this.responseText);
-  console.log(resultsArray);
-}
-
-makeAJAXRequest();
