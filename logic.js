0f52644f7e6d7fe4a8de0ef55cc24ce7d23831a6
wesley <wesleyyw8@gmail.com>
2018-10-11 22:54:21 +0100

    img according to button

--- logic.js
+++ logic.js
@@ -1,9 +1,24 @@
-var s = document.createElement("script");
-s.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=puppy";
-document.body.appendChild(s);
+var url = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=';
 
-//makeAJAXRequest();
+function makeRequest(name) {
+  var script = document.createElement("script");
+  script.src = url+name;
+  document.body.appendChild(script);
+  script.parentNode.removeChild(script);
+}
 
 function jsonFlickrFeed (data) {
-  console.log(data);
+  var imageContainer = document.getElementById('images-container')
+  while (imageContainer.firstChild) {
+    imageContainer.removeChild(imageContainer.firstChild);
+  }
+  var limit = data.items.length > 5 
+    ? 5 
+    : data.items.length;
+
+  for(var x=0; x<limit; x++) {
+    var img = document.createElement('img');
+    img.src = data.items[x].media.m;
+    document.getElementById('images-container').appendChild(img);
+  }
 }
